891a2fb362125a6136d4388b996a630fe0843d06
jhkim <jhkim@coasia.com>
2021-03-08 20:14:44 +0900

    firmware: init: set configure
    - SYSTEM_CLOCK  48000000
    - SYSTEM_TICK_HZ  1000
    
    Signed-off-by: jhkim <jhkim@coasia.com>

--- src/init.c
+++ src/init.c
@@ -47,7 +47,7 @@ static void HAL_SystemInit(void)
 {
 	System_Remap();
 	PLL_SetFrequency(SYSTEM_CLOCK);
-	SysTick_Init(SYS_TICK_HZ);
+	SysTick_Init(SYSTEM_TICK_HZ);
 
 	SYS_GpioInit();
 	UART_ConsoleInit(0, SYSTEM_CLOCK);
