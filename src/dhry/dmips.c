379b53751abd4334f6806d1f42bf24d7c33272bb
jhkim <jhkim@coasia.com>
2021-03-10 18:55:31 +0900

    firmware: add dhrystone
    Signed-off-by: jhkim <jhkim@coasia.com>

---
+++ src/dhry/dmips.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <config.h>
+
+#ifdef DMIPS_ENABLED
+
+#define _MHZ(_hz)	(_hz/1000000)
+
+extern void DMIPS(int number_of_runs, int cpu_mhz);
+
+static int do_dmips(int argc, char * const argv[])
+{
+	int number_of_runs = 20000;
+
+	if (argc > 1 && argv[1])
+		number_of_runs = strtol(argv[1], NULL, 10);
+
+	DMIPS(number_of_runs, _MHZ(SYSTEM_CLOCK));
+
+	return 0;
+}
+
+CMD_DEFINE(dmips, do_dmips);
+#endif
