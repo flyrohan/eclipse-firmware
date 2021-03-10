95b9efb0cf1e181fbea2ff447ae58494d0a1d0c8
jhkim <jhkim@coasia.com>
2021-03-10 17:01:40 +0900

    firmware: io: add mask macro.
    Signed-off-by: jhkim <jhkim@coasia.com>

--- system/include/nxve1000/io.h
+++ system/include/nxve1000/io.h
@@ -1,18 +1,22 @@
 #ifndef _IO_H_
 #define _IO_H_
 
-#include <cmsis_device.h>
+#define BIT(nr)         				(1UL << (nr))
+#define setbit(_a, _bit)				((_a) |= (1 << _bit))
+#define clrbit(_a, _bit)				((_a) &= ~(1 << _bit))
+
+#define readl(_a)						(*(volatile uint32_t *)(_a))
+#define writel(_v, _a)					(*(volatile uint32_t *)(_a) = (_v))
+
+#define _mask(_v, _m)					 (_v & ~(uint32_t)_m)
+#define _maskbits(_p, _w)               (((1 << _w) - 1) << _p)
+#define _getsftbits(_d, _p, _w)         (((_d) & _maskbits(_p, _w)) >> _p)
+#define _clrsetbits(_d, _p, _w, _v)		(((_d) & (uint32_t)~_maskbits(_p, _w)) | \
+										(uint32_t)(((_v) << _p) & _maskbits(_p, _w)))
+#define _writebits(_addr, _p, _w, _v)	writel(_clrsetbits(readl(_addr), _p, _w, _v), _addr)
+#define _readbits(_addr, _p, _w)		_getsftbits(readl(_addr), _p, _w)
 
 #define ARRAY_SIZE(x)	(int)(sizeof(x) / sizeof((x)[0]))
-
-#define readl(_a)		(*(volatile uint32_t *)(_a))
-#define writel(_v, _a)	(*(volatile uint32_t *)(_a) = (_v))
-
-#define _maskbits(_bp, _bw)                	(((1 << _bw) - 1) << _bp)
-#define _getsftbits(_in, _bp, _bw)         	(((_in) & _maskbits(_bp, _bw)) >> _bp)
-#define _clrsetbits(_in, _bp, _bw, _val)	(((_in) & (uint32_t)~_maskbits(_bp, _bw)) | (uint32_t)(((_val) << _bp) & _maskbits(_bp, _bw)))
-#define _writebits(_addr, _bp, _bw, _val)	writel(_clrsetbits(readl(_addr), _bp, _bw, _val), _addr)
-#define _readbits(_addr, _bp, _bw)			_getsftbits(readl(_addr), _bp, _bw)
 
 #endif
 
