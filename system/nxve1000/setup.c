d096ecc64038e5f53f8f5575f7ce2b0b33fac37f
jhkim <jhkim@coasia.com>
2021-03-31 11:20:13 +0900

    firmware: init: move src/init.c to system/nxve1000/setup.c
    Signed-off-by: jhkim <jhkim@coasia.com>

--- system/nxve1000/setup.c
+++ system/nxve1000/setup.c
@@ -32,10 +32,21 @@
 #include <init.h>
 #include <config.h>
 
-#ifdef HAL_INIT
-
 #define GPIO_UART_TX	24
 #define GPIO_UART_RX	25
+#define EFLASH_FIX_SPEEAD
+
+static void SYS_eFlashInit(void)
+{
+#ifdef EFLASH_FIX_SPEEAD
+	/* eflash timing: TRCK0, TRCK1, TWCMH, TWCK0, ERASE_READY_DOWN_ENB */
+	*(uint32_t *)0x42010018 = 0x0; // 0x9
+	*(uint32_t *)0x4201001c = 0x0; // 0x9
+	*(uint32_t *)0x42010028 = 0x0; // 0xD
+	*(uint32_t *)0x4201002c = 0x0; // 0x9
+	*(uint32_t *)0x4201004C = 0x1;
+#endif
+}
 
 static void SYS_GpioInit(void)
 {
@@ -44,20 +55,23 @@ static void SYS_GpioInit(void)
 }
 
 SYSTEM_INIT(HAL_SystemInit);
-void HAL_SystemInit(void)
+void __attribute__((weak)) HAL_SystemInit(void)
 {
 	System_Remap();
+
 	PLL_SetFrequency(SYSTEM_CLOCK);
-#ifdef SYSTICK_ENABLED
-	SysTick_Register(SYSTEM_CLOCK, SYSTEM_TICK_HZ);
-#endif
-#ifdef TIMER_ENABLED
-	TIMER_Register(0, SYSTEM_CLOCK, SYSTEM_TICK_HZ);
-#endif
+
+	SYS_eFlashInit();
 	SYS_GpioInit();
+
 #ifdef UART_ENABLED
 	UART_ConsoleRegister(0, SYSTEM_CLOCK);
 #endif
+
+#if defined(SYSTICK_ENABLED)
+	SysTick_Register(SYSTEM_CLOCK, SYSTEM_TICK_HZ);
+#elif defined(TIMER_ENABLED)
+	TIMER_Register(0, SYSTEM_CLOCK, SYSTEM_TICK_HZ);
+#endif
 }
 
-#endif
