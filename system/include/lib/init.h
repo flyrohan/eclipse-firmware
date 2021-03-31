756f3cc0c003b09db269c75086727305ee267dc4
jhkim <jhkim@coasia.com>
2021-03-31 11:22:03 +0900

    firmware: init: Fix SYSTEM_INIT call duplication
    Signed-off-by: jhkim <jhkim@coasia.com>

--- system/include/lib/init.h
+++ system/include/lib/init.h
@@ -5,7 +5,7 @@ typedef void (*initcall_t)(void);
 
 #if !defined(__ARMCC_VERSION)
 #define SYSTEM_INIT(fn) \
-	void fn(void) __attribute__((constructor)); \
+	void fn(void); \
 	initcall_t _init_##fn __attribute__ ((section(".init_array."#fn""))) = fn
 #else
 #define SYSTEM_INIT(fn) \
