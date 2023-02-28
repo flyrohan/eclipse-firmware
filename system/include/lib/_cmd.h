d20f6038e38b46bd62d983fdc1b3c7967aa66ba7
jhk <jhk@deepx.ai>
2023-02-28 13:08:41 +0900

    firmware: cmd: Suppot command help
    Signed-off-by: jhk <jhk@deepx.ai>

--- system/include/lib/_cmd.h
+++ system/include/lib/_cmd.h
@@ -6,6 +6,9 @@
 typedef struct __CMD_Entry {
     char *name;
     int	(*func)(int argc, char * const argv[]);
+#ifdef CMD_SUPPORT_HELP
+	const char *help;
+#endif
 #if defined(__ARMCC_VERSION)
     struct list_head list;
 #endif
@@ -15,18 +18,34 @@ int  CMD_Run(char *command);
 
 #if !defined(__ARMCC_VERSION)
 #define CMD_TYPE	static
+#ifdef CMD_SUPPORT_HELP
+#define CMD_DEFINE(_cmd, _func, _help) \
+	CMD_Entry _c_ent_##_func_##_cmd		\
+	__attribute__((unused, section(".cmd_entry_2_"#_cmd""))) = { \
+		.name = #_cmd, .func = _func, .help = _help }
+#else
 #define CMD_DEFINE(_cmd, _func) \
 	CMD_Entry _c_ent_##_func_##_cmd		\
 	__attribute__((unused, section(".cmd_entry_2_"#_cmd""))) = { #_cmd, _func }
+#endif
 #else
 void CMD_Register(CMD_Entry *entry);
 #define CMD_TYPE
+#ifdef CMD_SUPPORT_HELP
+#define CMD_DEFINE(_cmd, _func, _help) \
+static CMD_Entry _c_ent_##_cmd	= { #_cmd, _func, _help, LIST_HEAD_INIT(_c_ent_##_cmd.list) }; \
+void do_cmd_##_cmd(void) __attribute__((constructor(10)));	\
+void do_cmd_##_cmd(void) { \
+	CMD_Register(&_c_ent_##_cmd); \
+}
+#else
 #define CMD_DEFINE(_cmd, _func) \
 static CMD_Entry _c_ent_##_cmd	= { #_cmd, _func, LIST_HEAD_INIT(_c_ent_##_cmd.list) }; \
 void do_cmd_##_cmd(void) __attribute__((constructor(10)));	\
 void do_cmd_##_cmd(void) { \
 	CMD_Register(&_c_ent_##_cmd); \
 }
+#endif
 #endif
 
 #endif
--- system/lib/cmd.c
+++ system/lib/cmd.c
@@ -2,13 +2,13 @@
 #include <cmsis_device.h>
 #include <config.h>
 
-#ifdef CMD_ENABLED
+#ifdef CD_ENABLED
 
-#ifndef CMD_BUFFER_SIZE
-#define CMD_BUFFER_SIZE		128
+#ifndef CD_BUFFER_SIZE
+#define CD_BUFFER_SIZE		128
 #endif
-#ifndef CMD_MAXARGS
-#define CMD_MAXARGS		16
+#ifndef CD_AXARGS
+#define CD_AXARGS		16
 #endif
 
 #ifdef __DEBUG__
@@ -17,30 +17,31 @@
 #else
 #define dbg(m...)		do {} while(0)
 #endif
+#define msg(m...)		Printf(m)
 
-#if !defined(__ARMCC_VERSION)
-#define CMD_ENTRY_START \
+#if !defined(__ARCC_VERSION)
+#define CD_ENTRY_START \
 ({ \
         static char _cmd_start[0] __attribute__((unused, section(".cmd_entry_1"))); \
-        (CMD_Entry *)&_cmd_start; \
+        (CD_Entry *)&_cmd_start; \
 })
 
-#define CMD_ENTRY_END \
+#define CD_ENTRY_END \
 ({ \
         static char _cmd_end[0] __attribute__((unused, section(".cmd_entry_3"))); \
-        (CMD_Entry *)&_cmd_end; \
+        (CD_Entry *)&_cmd_end; \
 })
 
-static CMD_Entry *CMD_FindEntry(const char *cmd)
+static CD_Entry *CD_FindEntry(const char *cmd)
 {
-	CMD_Entry *start = CMD_ENTRY_START;
-	CMD_Entry *end = CMD_ENTRY_END;
+	CD_Entry *start = CD_ENTRY_START;
+	CD_Entry *end = CD_ENTRY_END;
 
 	dbg("Entry: cmd:%s, 0x%x-0x%x, %dEa\n", cmd, start, end, (end - start));
 	if (!cmd || (unsigned int)(end - start) == 0)
 		return NULL;
 
-	for (CMD_Entry *p = start; p < end; p++) {
+	for (CD_Entry *p = start; p < end; p++) {
 		if (strcmp(cmd, p->name) == 0) {
 			if (strlen(cmd) == strlen(p->name)) {
 				dbg("Found [%s:%s]\n", cmd, p->name);
@@ -55,12 +56,12 @@ static CMD_Entry *CMD_FindEntry(const char *cmd)
 #else
 static LIST_HEAD(cmd_entry_list);
 
-void CMD_Register(CMD_Entry *entry)
+void CD_Register(CD_Entry *entry)
 {
 	list_add(&entry->list, &cmd_entry_list);
 }
 
-CMD_Entry *CMD_FindEntry(const char *cmd)
+CD_Entry *CD_FindEntry(const char *cmd)
 {
 	struct list_head *pos;
 
@@ -68,7 +69,7 @@ CMD_Entry *CMD_FindEntry(const char *cmd)
 		return NULL;
 
 	list_for_each(pos, &cmd_entry_list) {
-		CMD_Entry *p = list_entry(pos, CMD_Entry, list);
+		CD_Entry *p = list_entry(pos, CD_Entry, list);
 		if (strcmp(cmd, p->name) == 0) {
 			if (strlen(cmd) == strlen(p->name)) {
 				dbg("Found [%s:%s]\n", cmd, p->name);
@@ -87,7 +88,7 @@ static int LineToArgs(char *line, char *argv[])
 {
 	int nargs = 0;
 
-	while (nargs < CMD_MAXARGS) {
+	while (nargs < CD_AXARGS) {
 		/* skip any white space */
 		while (isblank(*line))
 			++line;
@@ -114,9 +115,9 @@ static int LineToArgs(char *line, char *argv[])
 	return nargs;
 }
 
-static int CMD_RunEntry(int argc, char * const argv[])
+static int CD_RunEntry(int argc, char * const argv[])
 {
-	CMD_Entry *entry = CMD_FindEntry(argv[0]);
+	CD_Entry *entry = CD_FindEntry(argv[0]);
 
 	if (!entry)
 		return -1;
@@ -124,11 +125,11 @@ static int CMD_RunEntry(int argc, char * const argv[])
 	return (entry->func)(argc, argv);
 }
 
-int CMD_Run(char *cmd)
+int CD_Run(char *cmd)
 {
-	char cmdbuf[CMD_BUFFER_SIZE]; /* working copy of cmd */
+	char cmdbuf[CD_BUFFER_SIZE]; /* working copy of cmd */
 	char *str = cmdbuf;
-	char *argv[CMD_MAXARGS + 1];	/* NULL terminated	*/
+	char *argv[CD_AXARGS + 1];	/* NULL terminated	*/
 	char *token;			/* start of token in cmdbuf	*/
 	char *sep;			/* end of token (separator) in cmdbuf */
 	int argc, inquotes;
@@ -137,7 +138,7 @@ int CMD_Run(char *cmd)
 	if (!cmd || !*cmd)
 		return -1;
 
-	if (strlen(cmd) >= CMD_BUFFER_SIZE)
+	if (strlen(cmd) >= CD_BUFFER_SIZE)
 		return -1;
 
 	strcpy(cmdbuf, cmd);
@@ -172,10 +173,49 @@ int CMD_Run(char *cmd)
 			continue;
 		}
 
-		rc = CMD_RunEntry(argc, argv);
+		rc = CD_RunEntry(argc, argv);
 	}
 
 	return rc;
 }
 
+#ifdef CMD_SUPPORT_HELP
+static int do_cmd_help(int argc, char * argv[])
+{
+	CD_Entry *start = CD_ENTRY_START;
+	CD_Entry *end = CD_ENTRY_END;
+	char *cmd = NULL;
+
+	if ((unsigned int)(end - start) == 0)
+		return -1;
+
+	if (argc > 1)
+		cmd = argv[1];
+
+	for (CD_Entry *p = start; p < end; p++) {
+		if (!cmd) {
+			msg("\r%s\n", p->name);
+			if (p->help == NULL)
+				continue;
+			puts("\t");
+			puts(p->help);
+			puts("\n");
+			continue;
+		}
+
+		if (!strcmp(cmd, p->name) && strlen(cmd) == strlen(p->name)) {
+			msg("\r%s\n", p->name);
+			if (p->help == NULL)
+				continue;
+			puts("\t");
+			puts(p->help);
+			puts("\n");
+			return 0;
+		}
+	}
+
+	return 0;
+}
+CD_DEFINE(help, do_cmd_help, "help <cmd>");
+#endif
 #endif
